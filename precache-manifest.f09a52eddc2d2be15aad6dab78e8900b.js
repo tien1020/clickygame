26118589a33f65ade38dea6f91da91d1658215e9
Tien Nguyen <tien.ng1020@gmail.com>
2019-09-19 18:07:49 -0500

    Updates

---
+++ precache-manifest.f09a52eddc2d2be15aad6dab78e8900b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6650cd22b7f2023318ea115a60a05a88",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "d46c298b94e3e7428e55",
+    "url": "/clickygame/static/css/main.e63eea75.chunk.css"
+  },
+  {
+    "revision": "5e83aeb0a3d3423cfe0e",
+    "url": "/clickygame/static/js/2.80f08b19.chunk.js"
+  },
+  {
+    "revision": "d46c298b94e3e7428e55",
+    "url": "/clickygame/static/js/main.756f7444.chunk.js"
+  },
+  {
+    "revision": "5379ce6e23258ff101ed",
+    "url": "/clickygame/static/js/runtime~main.64b14ad1.js"
+  }
+]);
